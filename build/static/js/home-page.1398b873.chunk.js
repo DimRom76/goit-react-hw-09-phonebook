9de0514a5e1397026f46edd6ee05e05a0903ce4f
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2021-04-09 21:13:30 +0300

    build

---
+++ build/static/js/home-page.1398b873.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-09-phonebook"]=this["webpackJsonpgoit-react-hw-09-phonebook"]||[]).push([[2],{342:function(e,t,c){"use strict";c.r(t),c.d(t,"default",(function(){return r}));var n=c(3),o=(c(0),c(113));function r(){return Object(n.jsx)("div",{className:"container",children:Object(n.jsx)(o.a,{className:"paper",children:Object(n.jsx)("h1",{children:"\u0413\u043b\u0430\u0432\u043d\u0430\u044f"})})})}}}]);
+//# sourceMappingURL=home-page.1398b873.chunk.js.map
